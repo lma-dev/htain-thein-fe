2b290a84a8341dc6ece1c6df66a2dec8973f8315
LwinMoeAung <53222794+lwinmoeaung-springarts@users.noreply.github.com>
2024-01-16 09:23:30 +0900

    add package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    './node_modules/preline/preline.js',
-  ],
+  content: [  "./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {},
   },
-  plugins: [require('preline/plugin')],
+  plugins: [],
 }
+
