a11e155adac3bfd6642388f0b1a36e53933588a4
LwinMoeAung <53222794+lwinmoeaung-springarts@users.noreply.github.com>
2024-02-06 16:53:07 +0900

    add user profile dropdown

--- src/app/components/Navbar/page.jsx
+++ src/app/components/Navbar/page.jsx
@@ -1,6 +1,9 @@
+
 import Link from "next/link";
+import UserProfileIconDropdown from "../DropDown/UserProfileIconDropdown";
 
 const Navbar = () => {
+
     return (
         <div className="mt-5">
             <div className="sm:hidden">
@@ -16,36 +19,48 @@ const Navbar = () => {
 
             <div className="hidden sm:block">
                 <div className="border-b border-gray-200">
-                    <nav className="-mb-px flex gap-6 text-center align-middle justify-center" aria-label="Tabs">
-                        <a
-                            href="#"
-                            className="shrink-0 border-b-2 border-transparent px-1 pb-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-                        >
-                            Settings
-                        </a>
+                    <div className="flex justify-between px-10">
+                        <div className="flex">
+                            <Link href="/dashboard">Htain Thein</Link>
+                        </div>
+                        <div>
+                            <nav className="-mb-px flex gap-6 text-center align-middle justify-center" aria-label="Tabs">
+                                <a
+                                    href="#"
+                                    className="shrink-0 border-b-2 border-transparent px-1 pb-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                                >
+                                    Settings
+                                </a>
 
-                        <a
-                            href="#"
-                            className="shrink-0 border-b-2 border-transparent px-1 pb-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-                        >
-                            Messages
-                        </a>
+                                <a
+                                    href="#"
+                                    className="shrink-0 border-b-2 border-transparent px-1 pb-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                                >
+                                    Messages
+                                </a>
 
-                        <a
-                            href="#"
-                            className="shrink-0 border-b-2 border-transparent px-1 pb-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-                        >
-                            Archive
-                        </a>
+                                <a
+                                    href="#"
+                                    className="shrink-0 border-b-2 border-transparent px-1 pb-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                                >
+                                    Archive
+                                </a>
 
-                        <Link
-                            href="/notifications"
-                            className="shrink-0 border-b-2 border-sky-500 px-1 pb-4 text-sm font-medium text-sky-600"
-                            aria-current="page"
-                        >
-                            Notifications
-                        </Link>
-                    </nav>
+                                <Link
+                                    href="/notifications"
+                                    className="shrink-0 border-b-2 border-sky-500 px-1 pb-4 text-sm font-medium text-sky-600"
+                                    aria-current="page"
+                                >
+                                    Notifications
+                                </Link>
+                            </nav>
+                        </div>
+
+                        <div>
+                            <UserProfileIconDropdown />
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
