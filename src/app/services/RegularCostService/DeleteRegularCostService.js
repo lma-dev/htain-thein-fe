485a9727f41b79c4bad0ee47c0e9922372a75af7
LwinMoeAung <53222794+lwinmoeaung-springarts@users.noreply.github.com>
2024-02-26 11:43:57 +0900

    fix bug

--- src/app/services/RegularCostService/DeleteRegularCostService.js
+++ src/app/services/RegularCostService/DeleteRegularCostService.js
@@ -1,5 +1,5 @@
 import { deleteRegularCostApi } from "../../api/regular-cost/deleteRegularCostApi";
 
 export const deleteRegularCostService = async (regularCostId) => {
-  return await deleteRegularCostApi(`/general-outcome/${regularCostId}`);
+  return await deleteRegularCostApi(regularCostId);
 };
